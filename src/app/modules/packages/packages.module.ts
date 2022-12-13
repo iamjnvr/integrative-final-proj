0e9aeafbb04a1950bf4ac48a669b3c475f520d2b
Aldwinny <aldwin0313@yahoo.com.ph>
2022-12-14 04:00:39 +0800

    Cleanup and removal of package history component since it was not used

--- src/app/modules/packages/packages.module.ts
+++ src/app/modules/packages/packages.module.ts
@@ -1,17 +1,12 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PackageComponent } from 'src/app/components/package/package.component';
-import { PackageHistoryComponent } from 'src/app/components/package-history/package-history.component';
 import { PackageHotelComponent } from 'src/app/components/package-hotel/package-hotel.component';
 
 @NgModule({
-  declarations: [
-    PackageComponent,
-    PackageHistoryComponent,
-    PackageHotelComponent,
-  ],
+  declarations: [PackageComponent, PackageHotelComponent],
   imports: [CommonModule],
-  exports: [PackageComponent, PackageHistoryComponent, PackageHotelComponent],
+  exports: [PackageComponent, PackageHotelComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class PackagesModule {}
