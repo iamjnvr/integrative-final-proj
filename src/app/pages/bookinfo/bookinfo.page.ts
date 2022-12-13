94f68280e25f4439320fb83d13139acbcc81fa04
Aldwinny <aldwin0313@yahoo.com.ph>
2022-12-13 20:02:17 +0800

    Added description and fixed some bugs

--- src/app/pages/bookinfo/bookinfo.page.ts
+++ src/app/pages/bookinfo/bookinfo.page.ts
@@ -13,7 +13,7 @@ export class BookinfoPage implements OnInit {
       image:
         '../../../assets/media/hotels/batangas/microtel_wyndham_batangas/highres (2).jpg',
       description:
-        'Microtel Wyndham is a well-known hotel in Batangas. It is famous for its luxurious style foods as well as their great customer service.',
+        'Microtel Wyndham is a well-known hotel in Batangas. Every room is fitted with a private bathroom, coffee.tea machine and hair dryer. Superior rooms feature a spa bath. A TV is also provided.Complimentary breakfast to all guests. There is a 24-hour front desk, fitness centre, game room, banquet facilities and on-site parking at the property.',
     },
     {
       name: 'Birds of Paradise',
@@ -32,7 +32,7 @@ export class BookinfoPage implements OnInit {
       name: 'G1 Lodge',
       image: '../../../assets/media/hotels/baguio/G1_Lodge/highres (2).jpg',
       description:
-        'G1 Lodge is a well-known hotel in Baguio. It is famous for its luxurious style foods as well as their great customer service.',
+        'G1 Lodge is a well-known hotel in Baguio. It is famous for its luxurious style foods as well as their great customer service. G1 Lodge is set in Baguio, within 2.5 km of Camp John Hay and 3.4 km of Lourdes Grotto. Among the facilities of this property are a restaurant, a 24-hour front desk and room service, along with free WiFi throughout the property. The property offers car hire and features a garden and terrace.',
     },
   ];
   data!: any;
